36a5293357f734315acaf720f815fc62a88b9dc5
Brian Marquez I <brian3marquez@gmail.com>
2023-04-06 21:33:31 +0000

    Api

---
+++ FullStackAPI/FullStackAPI/Controllers/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStackAPI.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Controllers.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
