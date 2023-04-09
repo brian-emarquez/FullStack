87cf6bfe3f7bc62459e5fa30406b3c2ea94cce51
Brian3Marquez <brian3marquez@gmail.com>
2023-04-09 17:17:27 -0500

    ApiController

---
+++ FullStackAPI/FullStackAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            
+        }
+    }
+}
